4045761344e681cf680411bc014435d0b0dd2afb
agent47daun <agent47daun@gmail.com>
2022-04-13 19:59:30 +0400

    Переход к django-mptt для дерева модели Comment

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mptt import models as mptt_models
 
 # Create your models here.
 
@@ -7,7 +8,10 @@ class Post(models.Model):
     text = models.TextField(max_length=255)
 
 
-class Comment(models.Model):
+class Comment(mptt_models.MPTTModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    parent = models.ForeignKey("self", on_delete=models.CASCADE, related_name="children")
+    parent = mptt_models.TreeForeignKey("self", on_delete=models.CASCADE, related_name="children", null=True)
     text = models.TextField("Текст комментария", max_length=144)
+
+    class MPTTMeta:
+        order_insertion_by = ['text']
